6b4a5150debef376508b73a31f838e70d6b50ca4
Sandro Aparecido <60124712+Sandro0330@users.noreply.github.com>
2020-04-27 00:18:08 -0300

    Acrecentado um comanfo if

--- src/teste_git/Program.java
+++ src/teste_git/Program.java
@@ -5,6 +5,14 @@ public class Program {
 	public static void main(String[] args) {
 		
 		System.out.println("Boa noite!");
+		
+		System.out.println("Estudando  Git");
+		int n = 0;
+		if (n == 0) {
+			System.out.println("Indo bem! ");
+		}  else {
+			System.out.println("Indo mal!");
+		}
 
 	}
 
